6b4118443e85f2ff4abf9c76bf609acad52e64d7
lazysasha <oleksandr.shynkariuk@gmail.com>
2021-06-29 06:30:27 +0200

    implemented no cancellable coroutine using the GlobalScope

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,18 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val deferreds = repos.map { repo ->
+        GlobalScope.async {
+            service
+                .getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .bodyList()
+        }
+    }
+    return deferreds.awaitAll().flatten().aggregate()
 }
